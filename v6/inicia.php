121821a4f16f8383d1daf18af6166266a8b4bfd2
trarrigoni <tr.arrigoni@gmail.com>
2017-06-09 13:34:09 -0300

    Relatórios finais com pasta image e arquivo inicial de php

---
+++ v6/inicia.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('fpdf/fpdf.php');
+require_once('banco.php');
+require_once('oracle.php');
+require_once('relatorio.php');
+
+?>
---
+++ v6/principal.php
@@ -0,0 +1,23 @@
+<?php
+
+require('inicia.php');
+
+	$doc = new Relatorio();
+	
+	$filtro = 2;
+	$ocorrencia = 4;
+	
+	//$doc=$doc->relatorio_aluno_falta($filtro,$ocorrencia);
+	$doc=$doc->relatorio_turma_falta($filtro,$ocorrencia);
+
+	//$doc=$doc->relatorio_aluno_horario($filtro,$ocorrencia); // 3 -> entrada , 1 ou 2 -> saida
+	//$doc=$doc->relatorio_turma_horario($filtro,$ocorrencia);
+	
+	$doc->Output();
+
+?>
+
+
+
+
+
